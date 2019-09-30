13f95bf64a1de59bd1c2519fb14d20c529b61ada
Luke Waring <lukewaring@gmail.com>
2019-09-30 19:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;  
 }
 
+
+function takeANumber(katzDeliLine) {
+  let number = 
+  
+  katzDeliLine.push(number);
+  
+  return `Welcome, you are number ${number}.`;
+}
+
+
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return `There is nobody waiting to be served!`;
